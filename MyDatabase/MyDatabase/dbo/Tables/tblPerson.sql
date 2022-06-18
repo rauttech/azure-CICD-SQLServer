9f69fcc29b94e3373b731db706d9fce7025056dc
Vahid Farahmandian <farahmandian2011@gmail.com>
2022-06-18 16:44:33 +0430

    init

---
+++ MyDatabase/MyDatabase/dbo/Tables/tblPerson.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[tblPerson]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(50) NULL, 
+    [Age] INT NULL
+)
---
+++ MyDatabase/MyDatabase/dbo/Tables/tblUser.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[tblUser] (
+    [UserId]   INT          NOT NULL,
+    [UserName] VARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_tblUser] PRIMARY KEY CLUSTERED ([UserId] ASC)
+);
+
